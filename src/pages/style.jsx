95e353c6391e21c2c49a44cbf25b11031758aa3e
matheusadami.lima@gmail.com <matheusadami.lima@gmail.com>
2023-12-20 16:03:47 -0300

    projetos nextjs

---
+++ src/pages/style.jsx
@@ -0,0 +1,9 @@
+import style from './style.module.css';
+
+export default function Estiloso(){
+    return(
+        <div className={style.vermelho}>
+            <h1>Estilo usando CSS módulos</h1>
+        </div>
+    )
+}
